a283167bf7ae7cfe19e7d1fbafa14fcf3c2eb2fd
ravideep6399 <ravideepsingh1000@gmail.com>
2024-07-16 00:46:23 +0530

    push github 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:food_app/api/cookie_manager.dart';
+// import 'package:food_app/api/cookie_manager.dart';
 import 'package:food_app/api/firebase_api.dart';
 import 'package:food_app/firebase_options.dart';
 // import 'package:food_app/menu.dart';
-import 'package:food_app/navigation_controls.dart';
+// import 'package:food_app/navigation_controls.dart';
 import 'package:food_app/web_view_stack.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -29,7 +29,7 @@ class WebViewApp extends StatefulWidget {
 
 class _WebViewAppState extends State<WebViewApp> {
   late final WebViewController controller;
-  final CookieManager _cookieManager = CookieManager();
+  // final CookieManager _cookieManager = CookieManager();
   @override
   void initState() {
     super.initState();
@@ -43,24 +43,36 @@ class _WebViewAppState extends State<WebViewApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        actions: [
-          NavigationControls(controller: controller),
+      appBar: PreferredSize(
+        
+        preferredSize: const Size.fromHeight(20),
+        child: AppBar(
+          // actions: [
+          // NavigationControls(controller: controller),
           // Menu(controller: controller),
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () async {
-              await _cookieManager.clearCookies();
-              // Reload WebView or navigate to login screen
-              controller.clearCache();
-              controller.loadRequest(Uri.parse('https://demo.sayau.in/public/'));
-            },
-          ),
-        ],
-        backgroundColor: Colors.white,
+          // IconButton(
+          //   icon: Icon(Icons.logout),
+          //   onPressed: () async {
+          //     await _cookieManager.clearCookies();
+          //     // Reload WebView or navigate to login screen
+          //     controller.clearCache();
+          //     controller.loadRequest(Uri.parse('https://demo.sayau.in/public/'));
+          //   },
+          // ),
+          // ],
+          backgroundColor: Colors.white,
+        ),
       ),
-      body: WebViewStack(
-        controller: controller,
+      body: PopScope(
+        onPopInvoked: (value) async {
+          if (await controller.canGoBack()) {
+            await controller.goBack();
+          }
+        },
+        canPop: false,
+        child: WebViewStack(
+          controller: controller,
+        ),
       ),
     );
   }
